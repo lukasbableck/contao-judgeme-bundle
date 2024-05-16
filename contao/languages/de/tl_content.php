1869faf22434cdbaab2176f271b5f6c1aa9d75d3
Lukas Bableck <lukas@bableck.dev>
2024-05-16 10:34:57 +0200

    fix tl_content palette and translation

--- contao/languages/de/tl_content.php
+++ contao/languages/de/tl_content.php
@@ -2,6 +2,6 @@
 
 $GLOBALS['TL_LANG']['tl_content']['judgeme_legend'] = 'Judge.me';
 
-$GLOBALS['TL_LANG']['tl_content']['judgemeId'] = 'Judge.me Produkt ID';
+$GLOBALS['TL_LANG']['tl_content']['judgemeId'] = ['Judge.me Produkt ID', 'Geben Sie die Produkt ID von Judge.me ein.'];
 $GLOBALS['TL_LANG']['tl_content']['judgemeLimit'] = ['Anzahl der Bewertungen', 'Geben Sie die Anzahl der Bewertungen ein, die maximal angezeigt werden sollen.'];
 $GLOBALS['TL_LANG']['tl_content']['productPage'] = ['Produktseite', 'Geben Sie die URL der Produktseite ein.'];
