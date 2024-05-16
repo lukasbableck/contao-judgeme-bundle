f56b346c9a84edd1d20c75315b5c52f5c0b38bae
Lukas Bableck <lukas@bableck.dev>
2024-05-16 10:33:18 +0200

    add missing translations

--- contao/languages/de/default.php
+++ contao/languages/de/default.php
@@ -3,3 +3,6 @@
 $GLOBALS['TL_LANG']['MSC']['judgeme']['average'] = 'von 5 Sternen';
 $GLOBALS['TL_LANG']['MSC']['judgeme']['total'] = 'Bewertungen';
 $GLOBALS['TL_LANG']['MSC']['judgeme']['writeReview'] = 'Bewertung schreiben';
+
+$GLOBALS['TL_LANG']['CTE']['judgeme'] = 'Judge.me';
+$GLOBALS['TL_LANG']['CTE']['judgeme_reviews_element'] = ['Judge.me Bewertungen', 'Zeigt Judge.me Bewertungen an.'];
--- contao/languages/en/default.php
+++ contao/languages/en/default.php
@@ -3,3 +3,6 @@
 $GLOBALS['TL_LANG']['MSC']['judgeme']['average'] = 'of 5 stars';
 $GLOBALS['TL_LANG']['MSC']['judgeme']['total'] = 'reviews';
 $GLOBALS['TL_LANG']['MSC']['judgeme']['writeReview'] = 'Write a review';
+
+$GLOBALS['TL_LANG']['CTE']['judgeme'] = 'Judge.me';
+$GLOBALS['TL_LANG']['CTE']['judgeme_reviews_element'] = ['Judge.me Reviews', 'Display Judge.me reviews.'];
